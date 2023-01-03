4649d098735bbfd6cc575938fdcc33f428a337fa
Ushmita Dutta <ushmitakhushi11092000@gmail.com>
2023-01-03 11:57:27 +0530

    modification

--- index.js
+++ index.js
@@ -2,14 +2,21 @@ const express = require('express')
 const path=require('path')
 const app = express()
 const port = 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+/*const ushmitamiddleware=(req,res,next)=>{
+    console.log(req);
+    next();
+}**/
+app.use(express.static(path.join(__dirname,"public")))
+//app.use(ushmitamiddleware)
+app.get('/hello/:name', (req, res) => {
+  res.send('Hello World!'+req.params.name)
 })
 app.get('/about', (req, res) => {
     //res.send('About Page!')
-    res.sendFile(path.join(__dirname, 'index.html'))
-    })
+    //res.sendFile(path.join(__dirname, 'index.html'))
+    //res.status(500)
+    res.json({"ushmita":22});
+  })
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
