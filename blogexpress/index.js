0f68194755e996a0778e2e56d0fb3dc92620ead7
Ushmita Dutta <ushmitakhushi11092000@gmail.com>
2023-01-05 13:31:26 +0530

    Handlebars Implemented

--- blogexpress/index.js
+++ blogexpress/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const path=require('path')
+var exphbs= require('express-handlebars');
+
+//var app = express();
+
+
 const app = express()
 const port = 3000
+
+app.engine('handlebars',exphbs.engine());
+app.set('view engine','handlebars');
 
 app.use(express.static(path.join(__dirname,"static")))
 app.use('/',require(path.join(__dirname,'routes/blog.js')))
 
 app.listen(port, () => {
-  console.log(`Blog app listening on port ${port}`,)
+  console.log(`Blog app listening on port http://localhost:${port}`,)
 })
