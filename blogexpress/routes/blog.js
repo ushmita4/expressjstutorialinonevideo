d7a81971fd3c84611f20b13cbd38a88c29e8f497
Ushmita Dutta <ushmitakhushi11092000@gmail.com>
2023-01-06 11:11:03 +0530

    Added Blog Pages

--- blogexpress/routes/blog.js
+++ blogexpress/routes/blog.js
@@ -4,13 +4,15 @@ const router = express.Router()
 const blogs=require('../data/blogs')
 
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,'../templates/index.html'))
+  //res.sendFile(path.join(__dirname,'../templates/index.html'))
+  res.render('home');
 })
 router.get('/blog', (req, res) => {
  // blogs.forEach(e=>{
    // console.log(e.title);
   //});
-  res.sendFile(path.join(__dirname,'../templates/index.html'))
+  //res.sendFile(path.join(__dirname,'../templates/index.html'))
+  res.render('bloghome',{blogs:blogs});
 })
 router.get('/blogpost/:slug', (req, res) => {
 
@@ -18,7 +20,11 @@ router.get('/blogpost/:slug', (req, res) => {
    return e.slug==req.params.slug
   })
   console.log(myblog)
-  res.sendFile(path.join(__dirname,'../templates/blogpage.html'))
+  //res.sendFile(path.join(__dirname,'../templates/blogpage.html'))
+  res.render('blogpage',{
+    title:myblog[0].title,
+    content:myblog[0].content,
+  });
 })
 
 module.exports = router
